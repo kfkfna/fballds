a87e7e4c6225780d97f4873bcc29600c6af4ea2c
projectmanger <2415796068@qq.com>
2019-12-20 10:04:31 +0800

    第一次提交

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index'
+import './assets/fonts/iconfont.css'
+import './plugins/element.js'
+import './assets/css/global.css'
 
 Vue.config.productionTip = false
-
 new Vue({
   router,
   render: h => h(App)
